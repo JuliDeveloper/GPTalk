1d93d96647248926ffdea85576d57715f2549264
JuliaRomanenko <rja-97@yandex.ru>
2023-03-01 21:21:40 +0300

    Добавила кастомные картинки и цвета в проект

---
+++ GPTalk/Extension + UIColor.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+extension UIColor {
+    static var backgroundColor: UIColor { UIColor(named: "backgroundColor") ?? UIColor() }
+    static var progressColor: UIColor { UIColor(named: "progressColor") ?? UIColor() }
+}
